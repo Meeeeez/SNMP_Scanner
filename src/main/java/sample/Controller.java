a37986cee2a1aace522b2eaae31dc1c343e88367
Moritz Messner <stmesmor@bx.fallmerayer.it>
2020-12-16 17:56:40 +0100

    Added whole network scanning (incomplete & buggy)
    SNMP_Scanner can now scan a whole /32 network. It still crashes when
    trying to scan a /24 network.

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,7 +2,6 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
-
 import org.soulwing.snmp.*;
 import java.io.IOException;
 
@@ -15,6 +14,12 @@ public class Controller {
     public TextArea result_text_area;
     public Button scan_button;
     public TextArea event_log_text_area;
+
+    public TextField network_textfield_whole;
+    public TextField mask_textfield_whole;
+    public ChoiceBox<String> port_field_whole;
+    public ChoiceBox<String> community_field_whole;
+    public TextArea result_text_area_whole;
 
     private Mib mib;
 
@@ -31,6 +36,12 @@ public class Controller {
     */
     public void initialize() {
         scan_button.setDisable(true);
+        network_textfield_whole.setPromptText("Enter Network address");
+        mask_textfield_whole.setPromptText("Enter subnet mask");
+        port_field_whole.getItems().addAll("161", "162");
+        community_field_whole.getItems().addAll("public", "private");
+
+
         ip_address_field.setPromptText("Enter an IP-address");
         OID_field.setPromptText("Enter OID if using 'Scan'");
         port_field.getItems().addAll("161", "162");
@@ -48,7 +59,8 @@ public class Controller {
         target.setCommunity(snmp_community_field.getValue());
 
         try (SnmpContext context = SnmpFactory.getInstance().newContext(target, mib)) {
-            get_OID_response(OID_field.getText(), null, context);
+            String result_string = get_OID_response(OID_field.getText(), null, context);
+            result_text_area.appendText(result_string);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -63,11 +75,17 @@ public class Controller {
         target.setCommunity(snmp_community_field.getValue());
 
         try (SnmpContext context = SnmpFactory.getInstance().newContext(target, mib)) {
-            get_OID_response(".1.3.6.1.2.1.1.5.0", "Name: ",  context);   //name
-            get_OID_response(".1.3.6.1.2.1.1.6.0", "Location: ", context);    //location
-            get_OID_response(".1.3.6.1.2.1.25.1.1.0", "Uptime: ", context); //uptime
-            get_OID_response("1.3.6.1.2.1.25.1.6.0", "Running Processes: ", context); //Processes
-            get_OID_response(".1.3.6.1.2.1.1.1.0", "Hardware and Software: ", context);    //hardware
+            String result_string;
+            result_string = get_OID_response(".1.3.6.1.2.1.1.5.0", "Name: ",  context);   //name
+            result_text_area.appendText(result_string);
+            result_string = get_OID_response(".1.3.6.1.2.1.1.6.0", "Location: ", context);    //location
+            result_text_area.appendText(result_string);
+            result_string = get_OID_response(".1.3.6.1.2.1.25.1.1.0", "Uptime: ", context); //uptime
+            result_text_area.appendText(result_string);
+            result_string = get_OID_response("1.3.6.1.2.1.25.1.6.0", "Running Processes: ", context); //Processes
+            result_text_area.appendText(result_string);
+            result_string = get_OID_response(".1.3.6.1.2.1.1.1.0", "Hardware and Software: ", context);    //hardware
+            result_text_area.appendText(result_string);
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
@@ -75,13 +93,13 @@ public class Controller {
         }
     }
 
-    private void get_OID_response(String OID, String OID_description, SnmpContext context){
+    private String get_OID_response(String OID, String OID_description, SnmpContext context){
         SnmpResponse<VarbindCollection> response = context.get(OID);
         VarbindCollection result = response.get();
         if(OID_description == null){
-            result_text_area.appendText(result.get(0).toString() + "\n");
+            return result.get(0).toString() + "\n";
         }else{
-            result_text_area.appendText(OID_description + result.get(0).toString() + "\n");
+            return OID_description + result.get(0).toString() + "\n";
         }
     }
 
@@ -107,5 +125,47 @@ public class Controller {
             e.printStackTrace();
         }
         return mib;
+    }
+
+    public void scan_whole_network(ActionEvent actionEvent) {
+        int mask = Integer.parseInt(mask_textfield_whole.getText());
+        String IP_new;
+
+        mib = load_MIB();
+
+        if(mask == 32){
+            SimpleSnmpV2cTarget target = new SimpleSnmpV2cTarget();
+            target.setAddress(network_textfield_whole.getText());
+            target.setPort(Integer.parseInt(port_field_whole.getValue()));
+            target.setCommunity(community_field_whole.getValue());
+            try (SnmpContext context = SnmpFactory.getInstance().newContext(target, mib)) {
+                String result_String = get_OID_response(".1.3.6.1.2.1.1.5.0", "Name: ",  context);   //name
+                result_text_area_whole.appendText(result_String);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else if(mask == 24){
+            String[] IP_address_arr = network_textfield_whole.getText().split("\\.");
+            System.out.println(network_textfield_whole.getText());
+            System.out.println(IP_address_arr.length);
+
+            for (int i = 0; i < 255; i++){
+                SimpleSnmpV2cTarget target = new SimpleSnmpV2cTarget();
+                IP_new = IP_address_arr[0] + "." + IP_address_arr[1] + "." + IP_address_arr[2] + "." + i;
+                System.out.println(IP_new);
+                target.setAddress(IP_new);
+
+                target.setPort(Integer.parseInt(port_field_whole.getValue()));
+                target.setCommunity(community_field_whole.getValue());
+
+                try (SnmpContext context = SnmpFactory.getInstance().newContext(target, mib)) {
+                    String result_String = get_OID_response(".1.3.6.1.2.1.1.5.0", "Name: ",  context);   //name
+                    result_text_area_whole.appendText(IP_new + ": " + result_String);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
