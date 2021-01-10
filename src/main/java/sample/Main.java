0a44a8a5a434fbbf02b994f11838fe5054c128c0
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-01-10 22:31:24 +0100

    Fixed the problem with sample.fxml (Error: location is required)

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,16 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.File;
-import java.net.URL;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        URL url = new File("src\\main\\java\\sample\\sample.fxml").toURI().toURL();
+        Parent root;
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/sample.fxml"));
+        root = fxmlLoader.load();
 
-        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("SNMP Scanner");
         primaryStage.setScene(new Scene(root, 520, 430));
         primaryStage.show();
